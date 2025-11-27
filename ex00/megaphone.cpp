30b164c2fa5109aa16a2371bce3c0373a66e321d
Jenny Bellucci <jbellucc@e4r10p2.42roma.it>
2025-11-27 15:04:46 +0100

    inizio

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbellucc <jbellucc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 14:08:38 by jbellucc          #+#    #+#             */
+/*   Updated: 2025/11/27 14:28:29 by jbellucc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+//#include <algorithm>
+
+int main(int ac, char **av)
+{
+    av = 0;
+    
+    if (ac <= 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (0);
+    }
+}
